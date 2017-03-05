7c9cc16a08d5eef9a000349ab31b5e2164bed218
Ric17101 <ric17101raguine@gmail.com>
2017-03-05 22:57:49 +0800

    At home UI fix

---
+++ signature_pad_library/build/generated/source/dataBinding/release/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="7e2b3887-3d06-4bfa-a3af-cc8a6e664f23", modulePackage="com.github.gcacace.signaturepad", sdkRoot="C:\\Users\\RIchard\\AppData\\Local\\Android\\Sdk",layoutInfoDir="C:\\Users\\RIchard\\PROJECT\\navigation-fragmentv2\\signature_pad_library\\build\\intermediates\\data-binding-info\\release",exportClassListTo="C:\\Users\\RIchard\\PROJECT\\navigation-fragmentv2\\signature_pad_library\\build\\intermediates\\data-binding-info\\release\\_generated.txt",isLibrary=true,minSdk=9,enableDebugLogs=false,printEncodedError=true)
+public class DataBindingInfo {}
