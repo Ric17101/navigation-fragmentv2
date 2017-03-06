79210e5e3422248bb37da48d004cce15b1105c96
admin 4 <ric17101raguine@gmail.com>
2017-03-06 19:29:37 +0800

    Dialog Box Fix using Custom Material Dialog Box and util class generation

--- signature_pad_library/build/generated/source/dataBinding/androidTest/debug/android/databinding/layouts/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package android.databinding.layouts;
-
-import android.databinding.BindingBuildInfo;
-
-@BindingBuildInfo(buildId="1565db64-4674-43ad-bdda-e987d74c0abc", modulePackage="com.github.gcacace.signaturepad.test", sdkRoot="C:\\Users\\RIchard\\AppData\\Local\\Android\\Sdk",layoutInfoDir="C:\\Users\\RIchard\\PROJECT\\navigation-fragmentv2\\signature_pad_library\\build\\intermediates\\data-binding-info\\androidTest\\debug",exportClassListTo="",isLibrary=false,minSdk=9,enableDebugLogs=false,printEncodedError=true)
-public class DataBindingInfo {}
--- signature_pad_library/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package android.databinding.layouts;
-
-import android.databinding.BindingBuildInfo;
-
-@BindingBuildInfo(buildId="f88fea5c-5812-41e4-b5bc-ad6b69c8a933", modulePackage="com.github.gcacace.signaturepad", sdkRoot="C:\\Users\\RIchard\\AppData\\Local\\Android\\Sdk",layoutInfoDir="C:\\Users\\RIchard\\PROJECT\\navigation-fragmentv2\\signature_pad_library\\build\\intermediates\\data-binding-info\\debug",exportClassListTo="C:\\Users\\RIchard\\PROJECT\\navigation-fragmentv2\\signature_pad_library\\build\\intermediates\\data-binding-info\\debug\\_generated.txt",isLibrary=true,minSdk=9,enableDebugLogs=false,printEncodedError=true)
-public class DataBindingInfo {}
