3652c99506681420f719de07f4d01019858d3fde
admin 4 <ric17101raguine@gmail.com>
2017-05-12 10:51:08 +0800

    Http Simple Authentication

---
+++ signature_pad_library/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="8c1408b1-2720-4b88-9174-995cc8a60d00")
+public class DataBindingInfo {}
---
+++ signature_pad_library/build/generated/source/dataBinding/release/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="d7aa9778-c082-4487-b5c2-1ec4fbaebdd9")
+public class DataBindingInfo {}
