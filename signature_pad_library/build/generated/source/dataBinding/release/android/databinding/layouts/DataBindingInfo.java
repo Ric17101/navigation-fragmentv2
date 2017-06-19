693837ac6ac13922cd0dbf6bcb05f27f86488ae8
admin 4 <ric17101raguine@gmail.com>
2017-06-19 14:07:41 +0800

    Monday Merge, Project Job Drawing (Patrick ) 2PM

---
+++ signature_pad_library/build/generated/source/dataBinding/release/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="8bfe45fe-dcb4-4425-8805-1398b1ec76ed")
+public class DataBindingInfo {}
